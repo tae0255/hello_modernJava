c7808513adda088154b2da9d1d7816805a27146e
solar-beam <xmckx0831@gmail.com>
2021-11-30 17:01:17 +0900

    initial commit

---
+++ JavaEight/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args){
+        //File[] hiddenFiles = new File("C:\\").listFiles(File::isHidden);
+        //System.out.println(Arrays.stream(hiddenFiles).toArray().toString());
+
+
+
+    }
+}
