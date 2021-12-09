ae2ce9879176b500233f6ecda7f449ac57d55a71
solar-beam <xmckx0831@gmail.com>
2021-12-09 09:31:48 +0900

    Update main
    this keyword scope exercise solution

--- JavaEleven/src/Main.java
+++ JavaEleven/src/Main.java
@@ -4,9 +4,30 @@ import Chapter02.RunnableExercise;
 
 public class Main {
     public static void main(String[] args){
-        new ComparatorExercise()._test();
-        new RunnableExercise()._test();
-        new CallableExercise()._test();
+        //new ComparatorExercise()._test();
+        //new RunnableExercise()._test();
+        //new CallableExercise()._test();
+
+        MeaningOfThis m = new MeaningOfThis();
+        m.doIt();
+    }
+
+    public static class MeaningOfThis{
+        public final int value = 4;
+        public void doIt(){
+            int value = 6;
+            Runnable r = new Runnable() {
+                public final int value = 5;
+                @Override
+                public void run() {
+                    int value = 10;
+                    System.out.println(value);
+                    System.out.println(this.value);
+                }
+            };
+            r.run();
+        }
 
     }
+
 }
